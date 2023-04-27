3c1245fa8b86822d969ab8eff5285186b4d1df9f
vahid-nejad <vahid884@gmail.com>
2023-04-27 08:58:18 +0330

    first commit

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
 
-module.exports = nextConfig
+  images: {
+    domains: ["picsum.photos"],
+  },
+};
+
+module.exports = nextConfig;
---
+++ src/components/AppBar.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import React from "react";
+
+const AppBar = () => {
+  return (
+    <header className="flex gap-4 p-4 bg-gradient-to-b from-white to-gray-200 shadow">
+      <Link className="transition-colors hover:text-blue-500" href={"/"}>
+        Home Page
+      </Link>
+      <Link className="transition-colors hover:text-blue-500" href={"/UserPost"}>
+        User Post Page
+      </Link>
+    </header>
+  );
+};
+
+export default AppBar;
